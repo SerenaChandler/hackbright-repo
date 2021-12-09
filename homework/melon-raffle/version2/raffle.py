11398611e57f35ba0ca1ce1bc1328c4de056e38b
Serena Chandler <serenachandler2019@gmail.com>
2021-12-09 02:35:08 +0000

    finished up raffle version 2

--- homework/melon-raffle/version2/raffle.py
+++ homework/melon-raffle/version2/raffle.py
@@ -1,6 +1,18 @@
 """Randomly pick customer and print customer info"""
 
 # Add code starting here
-
+import customers
+import random
 # Hint: remember to import any functions you need from
 # other files or libraries
+def pick_winner(customers):
+    winner = random.choice(customers)
+
+    name = winner.name
+    email = winner.email
+
+    print(f"the winner is... {name} at {email}!!!")
+
+def start_raffle():
+    customer = customers.get_customers_from_file("customers.txt")
+    pick_winner(customer)
