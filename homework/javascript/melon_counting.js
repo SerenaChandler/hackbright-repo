4b62f494afe2c69be762b192e6171fb0e150faf0
Serena Chandler <serenachandler2019@gmail.com>
2021-12-21 00:34:32 +0000

    finished javascript hw

---
+++ homework/javascript/melon_counting.js
@@ -0,0 +1,27 @@
+melons_to_add = ['Ogen', 'Horned Melon', 'Watermelon', 'Casaba',
+                 'Sharlyn', 'Xigua', 'Ogen', 'Christmas', 'Christmas',
+                 'Christmas', 'Christmas', 'Watermelon', 'Sharlyn', 'Xigua',
+                 'Cantaloupe', 'Christmas', 'Watermelon', 'Christmas',
+                 'Sharlyn', 'Christmas', 'Cantaloupe', 'Casaba', 'Cantaloupe',
+                 'Santa Claus', 'Horned Melon', 'Watermelon', 'Ogen',
+                 'Horned Melon', 'Cantaloupe', 'Xigua', 'Horned Melon', 'Sharlyn',
+                 'Horned Melon', 'Sharlyn', 'Cantaloupe', 'Christmas',
+                 'Horned Melon', 'Horned Melon', 'Horned Melon', 'Xigua', 'Xigua',
+                 'Watermelon', 'Cantaloupe', 'Casaba', 'Cantaloupe', 'Casaba',
+                 'Watermelon', 'Santa Claus', 'Casaba']
+
+const count_melons = (melon_list) => {
+  melon_counts = {};
+
+  for (melon of melon_list) {
+    if (melon_counts.hasOwnProperty(melon)) {
+      melon_counts[melon] = melon_counts[melon] + 1;
+    } else {
+      melon_counts[melon] = 1;
+    }
+  }
+  console.log(melon_counts)
+  return melon_counts;
+};
+
+count_melons(melons_to_add)
