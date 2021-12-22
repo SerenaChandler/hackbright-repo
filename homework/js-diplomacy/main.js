b69789e76ce8b095393a34ef0c7a2a82a3d3d7a9
Serena Chandler <serenachandler2019@gmail.com>
2021-12-22 02:16:43 +0000

    finished JS dom manipulation hw

--- homework/js-diplomacy/main.js
+++ homework/js-diplomacy/main.js
@@ -1,1 +1,25 @@
-'use strict';
+"use strict";
+const color_button = document.querySelector(".color-changer");
+
+color_button.addEventListener("click", () => {
+  console.log("hello");
+  const elements = document.querySelectorAll(".color-change");
+  console.log(elements);
+  for (const element of elements) {
+    element.classList.add("class", "red");
+  }
+});
+
+const input_form = document.querySelector(".number-form");
+
+input_form.addEventListener("click", (event) => {
+  event.preventDefault();
+  const num_input = document.querySelector('#number-input').value;
+  const feedback = document.querySelector("#formFeedback");
+  console.log(num_input);
+  if (parseInt(num_input) >= 10 || isNaN(num_input)) {
+      feedback.textContent = "Please enter a smaller number";
+  } else {
+    feedback.innerHTML = "you are good to go!";
+  }
+});
