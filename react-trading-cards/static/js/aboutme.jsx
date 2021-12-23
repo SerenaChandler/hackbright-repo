4b48f21bbea4b1693615582e0e674534e02719f3
Serena Chandler <serenachandler2019@gmail.com>
2021-12-23 00:46:19 +0000

    completed further study

---
+++ react-trading-cards/static/js/aboutme.jsx
@@ -0,0 +1,11 @@
+'use strict';
+
+
+function AboutMe(){
+
+    return (
+        <h2>This is made by Serena and Jenny</h2>
+
+    )
+}
+ReactDOM.render(<AboutMe />, document.querySelector('#about'));
--- react-trading-cards/static/js/homepage.jsx
+++ react-trading-cards/static/js/homepage.jsx
@@ -6,6 +6,7 @@ function Homepage() {
   <div>
   <a href = '/cards'>Click here to see your card</a>
   <img src = '/static/img/balloonicorn.jpg'/>
+  <a href = '/about'>Click here to see about me</a>
   </div>
   )}
 
